885be7ec42e513a3ad3a633bfc848a9ac0d849a7
Muzamil Satar <muzamil@mzml.io>
2018-04-27 21:36:17 -0600

    a driver class

--- src/test/java/steps/Driver.java
+++ src/test/java/steps/Driver.java
@@ -1,8 +1,15 @@
 package steps;
 
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.firefox.FirefoxProfile;
@@ -10,31 +17,31 @@ import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.ie.InternetExplorerOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.safari.SafariDriver;
+import org.openqa.selenium.safari.SafariOptions;
 
+import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.ios.IOSDriver;
 
-import java.io.FileInputStream;
-import java.net.URL;
-import java.util.*;
-import java.util.concurrent.TimeUnit;
-
 /**
- * Selenium Driver Class
+ * 
+ * @author mzml
+ *
  */
 public class Driver {
 	// local variables
 	private static Driver instance = null;
+	private String browserHandle = null;
 	private static final int IMPLICIT_TIMEOUT = 0;
 	private ThreadLocal<WebDriver> webDriver = new ThreadLocal<WebDriver>();
+	private ThreadLocal<AppiumDriver<MobileElement>> mobileDriver = new ThreadLocal<AppiumDriver<MobileElement>>();
 	private ThreadLocal<String> sessionId = new ThreadLocal<String>();
 	private ThreadLocal<String> sessionBrowser = new ThreadLocal<String>();
 	private ThreadLocal<String> sessionPlatform = new ThreadLocal<String>();
 	private ThreadLocal<String> sessionVersion = new ThreadLocal<String>();
 	private String getEnv = null;
-	private Properties props = new Properties();
-	private ThreadLocal<WebDriver> mobileDriver;
 
 	// constructor
 	private Driver() {
@@ -54,7 +61,7 @@ public class Driver {
 	}
 
 	/**
-	 * setDriver method to create driver instance
+	 * setDriver method
 	 *
 	 * @param browser
 	 * @param environment
@@ -63,12 +70,32 @@ public class Driver {
 	 * @throws Exception
 	 */
 	@SafeVarargs
-	public final void setDriver(String browser, String platform, String environment,
+	public final void setDriver(String browser, String environment, String platform,
 			Map<String, Object>... optPreferences) throws Exception {
 
 		DesiredCapabilities caps = null;
+		String localHub = "http://127.0.0.1:4723/wd/hub";
 		String getPlatform = null;
-		props.load(new FileInputStream("Global_VARS.SE_PROPS"));
+
+		getEnv = "local";
+		getPlatform = platform;
+
+		if (browser.equalsIgnoreCase("iphone") || browser.equalsIgnoreCase("android")) {
+
+			sessionId.set(((IOSDriver<MobileElement>) mobileDriver.get()).getSessionId().toString());
+
+			sessionId.set(((AndroidDriver<MobileElement>) mobileDriver.get()).getSessionId().toString());
+
+			sessionBrowser.set(browser);
+			sessionVersion.set(caps.getCapability("deviceName").toString());
+			sessionPlatform.set(getPlatform);
+		} else {
+			sessionId.set(((RemoteWebDriver) webDriver.get()).getSessionId().toString());
+
+			sessionBrowser.set(caps.getBrowserName());
+			sessionVersion.set(caps.getVersion());
+			sessionPlatform.set(getPlatform);
+		}
 
 		switch (browser) {
 		case "firefox":
@@ -76,20 +103,19 @@ public class Driver {
 
 			FirefoxOptions ffOpts = new FirefoxOptions();
 			FirefoxProfile ffProfile = new FirefoxProfile();
-
 			ffProfile.setPreference("browser.autofocus", true);
-			ffProfile.setPreference("browser.tabs.remote.autostart.2", false);
 
 			caps.setCapability(FirefoxDriver.PROFILE, ffProfile);
 			caps.setCapability("marionette", true);
-
-			// then pass them to the local WebDriver
-			if (environment.equalsIgnoreCase("local")) {
-				System.setProperty("webdriver.gecko.driver", props.getProperty("gecko.driver.windows.path"));
-				webDriver.set(new FirefoxDriver(ffOpts.merge(caps)));
+			if (optPreferences.length > 0) {
+				processFFProfile(ffProfile, optPreferences);
 			}
+			webDriver.set(new FirefoxDriver(caps));
 
+			// Selenium 3.7.x
+			// webDriver.set(new FirefoxDriver(ffOpts.merge(caps)));
 			break;
+
 		case "chrome":
 			caps = DesiredCapabilities.chrome();
 
@@ -97,35 +123,71 @@ public class Driver {
 			Map<String, Object> chromePrefs = new HashMap<String, Object>();
 
 			chromePrefs.put("credentials_enable_service", false);
-
 			chOptions.setExperimentalOption("prefs", chromePrefs);
 			chOptions.addArguments("--disable-plugins", "--disable-extensions", "--disable-popup-blocking");
 
 			caps.setCapability(ChromeOptions.CAPABILITY, chOptions);
 			caps.setCapability("applicationCacheEnabled", false);
-
-			if (environment.equalsIgnoreCase("local")) {
-				System.setProperty("webdriver.chrome.driver", props.getProperty("chrome.driver.windows.path"));
-				webDriver.set(new ChromeDriver(chOptions.merge(caps)));
+			if (optPreferences.length > 0) {
+				processCHOptions(chOptions, optPreferences);
 			}
+			webDriver.set(new ChromeDriver(caps));
+
+			// Selenium 3.7.x
+			// webDriver.set(new ChromeDriver(chOptions.merge(caps)));
 
 			break;
+
 		case "internet explorer":
 			caps = DesiredCapabilities.internetExplorer();
 
 			InternetExplorerOptions ieOpts = new InternetExplorerOptions();
-
 			ieOpts.requireWindowFocus();
+
 			ieOpts.merge(caps);
-
 			caps.setCapability("requireWindowFocus", true);
-
-			if (environment.equalsIgnoreCase("local")) {
-				System.setProperty("webdriver.ie.driver", props.getProperty("ie.driver.windows.path"));
-				webDriver.set(new InternetExplorerDriver(ieOpts.merge(caps)));
+			if (optPreferences.length > 0) {
+				processDesiredCaps(caps, optPreferences);
 			}
+			webDriver.set(new InternetExplorerDriver(caps));
+
+			// Selenium 3.7.x
+			// webDriver.set(new InternetExplorerDriver(ieOpts.merge(caps)));
+			break;
+
+		case "safari":
+			caps = DesiredCapabilities.safari();
+			caps = DesiredCapabilities.safari();
+
+			SafariOptions safariOpts = new SafariOptions();
+			safariOpts.setUseCleanSession(true);
+
+			caps.setCapability(SafariOptions.CAPABILITY, safariOpts);
+			caps.setCapability("autoAcceptAlerts", true);
+
+			webDriver.set(new SafariDriver(caps));
+
+			// Selenium 3.7.x
+			// webDriver.set(new SafariDriver(safariOpts.merge(caps)));
 
 			break;
+
+		case "microsoftedge":
+			caps = DesiredCapabilities.edge();
+			caps = DesiredCapabilities.edge();
+
+			EdgeOptions edgeOpts = new EdgeOptions();
+			edgeOpts.setPageLoadStrategy("normal");
+
+			caps.setCapability(EdgeOptions.CAPABILITY, edgeOpts);
+			caps.setCapability("requireWindowFocus", true);
+
+			webDriver.set(new EdgeDriver(caps));
+
+			// Selenium 3.7.x
+			// webDriver.set(new EdgeDriver(edgeOpts.merge(caps)));
+			break;
+
 		case "iphone":
 		case "ipad":
 			if (browser.equalsIgnoreCase("ipad")) {
@@ -142,36 +204,60 @@ public class Driver {
 			caps.setCapability("device", "iPhone"); // or iPad
 
 			mobileDriver.set(new IOSDriver<MobileElement>(new URL("http://127.0.0.1:4723/wd/hub"), caps));
-
 			break;
+
 		case "android":
 			caps = DesiredCapabilities.android();
 
 			caps.setCapability("appName", "https://myapp.com/myApp.apk");
 
 			caps.setCapability("udid", "12345678"); // physical device
-			caps.setCapability("device", "android");
+			caps.setCapability("device", "Android");
+
 			mobileDriver.set(new AndroidDriver<MobileElement>(new URL("http://127.0.0.1:4723/wd/hub"), caps));
 			break;
 		}
 
-		getEnv = environment;
-		getPlatform = platform;
-		sessionId.set(((RemoteWebDriver) webDriver.get()).getSessionId().toString());
-		sessionBrowser.set(caps.getBrowserName());
-		sessionVersion.set(caps.getVersion());
-		sessionPlatform.set(getPlatform);
-
-		System.out.println("\n*** TEST ENVIRONMENT = " + getSessionBrowser().toUpperCase() + "/"
-				+ getSessionPlatform().toUpperCase() + "/" + getEnv.toUpperCase() + "/Selenium Version="
-				+ props.getProperty("selenium.revision") + "/Session ID=" + getSessionId() + "\n");
-
-		getDriver().manage().timeouts().implicitlyWait(IMPLICIT_TIMEOUT, TimeUnit.SECONDS);
-		getDriver().manage().window().maximize();
 	}
 
 	/**
-	 * getDriver method to retrieve active driver
+	 * overloaded setDriver method to switch driver to specific WebDriver if running
+	 * concurrent drivers
+	 *
+	 * @param driver
+	 *            WebDriver instance to switch to
+	 */
+	public void setDriver(WebDriver driver) {
+		webDriver.set(driver);
+
+		sessionId.set(((RemoteWebDriver) webDriver.get()).getSessionId().toString());
+
+		sessionBrowser.set(((RemoteWebDriver) webDriver.get()).getCapabilities().getBrowserName());
+
+		sessionPlatform.set(((RemoteWebDriver) webDriver.get()).getCapabilities().getPlatform().toString());
+
+		setBrowserHandle(getDriver().getWindowHandle());
+	}
+
+	/**
+	 * overloaded setDriver method to switch driver to specific AppiumDriver if
+	 * running concurrent drivers
+	 *
+	 * @param driver
+	 *            AppiumDriver instance to switch to
+	 */
+	public void setDriver(AppiumDriver<MobileElement> driver) {
+		mobileDriver.set(driver);
+
+		sessionId.set(mobileDriver.get().getSessionId().toString());
+
+		sessionBrowser.set(mobileDriver.get().getCapabilities().getBrowserName());
+
+		sessionPlatform.set(mobileDriver.get().getCapabilities().getPlatform().toString());
+	}
+
+	/**
+	 * getDriver method will retrieve the active WebDriver
 	 *
 	 * @return WebDriver
 	 */
@@ -180,12 +266,62 @@ public class Driver {
 	}
 
 	/**
-	 * closeDriver method to close active driver
+	 * getDriver method will retrieve the active AppiumDriver
 	 *
+	 * @param mobile
+	 *            boolean parameter
+	 * @return AppiumDriver
+	 */
+	public AppiumDriver<MobileElement> getDriver(boolean mobile) {
+		return mobileDriver.get();
+	}
+
+	/**
+	 * getCurrentDriver method will retrieve the active WebDriver or AppiumDriver
+	 *
+	 * @return WebDriver
+	 */
+	public WebDriver getCurrentDriver() {
+		if (getInstance().getSessionBrowser().contains("iphone") || getInstance().getSessionBrowser().contains("ipad")
+				|| getInstance().getSessionBrowser().contains("android")) {
+
+			return getInstance().getDriver(true);
+		}
+
+		else {
+			return getInstance().getDriver();
+		}
+	}
+
+	/**
+	 * driverWait method pauses the driver in seconds
+	 *
+	 * @param seconds
+	 *            to pause
+	 */
+	public void driverWait(long seconds) {
+		try {
+			Thread.sleep(TimeUnit.SECONDS.toMillis(seconds));
+		}
+
+		catch (InterruptedException e) {
+			// do something
+		}
+	}
+
+	/**
+	 * driverRefresh method reloads the current browser page
+	 */
+	public void driverRefresh() {
+		getCurrentDriver().navigate().refresh();
+	}
+
+	/**
+	 * closeDriver method quits the current active driver
 	 */
 	public void closeDriver() {
 		try {
-			getDriver().quit();
+			getCurrentDriver().quit();
 		}
 
 		catch (Exception e) {
@@ -194,43 +330,41 @@ public class Driver {
 	}
 
 	/**
-	 * getSessionId method to retrieve active id
+	 * getSessionId method gets the browser or mobile id of the active session
 	 *
 	 * @return String
-	 * @throws Exception
 	 */
-	public String getSessionId() throws Exception {
+	public String getSessionId() {
 		return sessionId.get();
 	}
 
 	/**
-	 * getSessionBrowser method to retrieve active browser
-	 * 
+	 * getSessionBrowser method gets the browser or mobile type of the active
+	 * session
+	 *
 	 * @return String
-	 * @throws Exception
 	 */
-	public String getSessionBrowser() throws Exception {
+	public String getSessionBrowser() {
 		return sessionBrowser.get();
 	}
 
 	/**
-	 * getSessionVersion method to retrieve active version
-	 *
+	 * getSessionVersion method gets the browser or mobile version of the active
+	 * session
+	 * 
 	 * @return String
-	 * @throws Exception
 	 */
-	public String getSessionVersion() throws Exception {
+	public String getSessionVersion() {
 		return sessionVersion.get();
 	}
 
 	/**
-	 * getSessionPlatform method to retrieve active platform
-	 * 
+	 * getSessionPlatform method gets the browser or mobile platform of the active
+	 * session
+	 *
 	 * @return String
-	 * @throws Exception
 	 */
-	public String getSessionPlatform() throws Exception {
+	public String getSessionPlatform() {
 		return sessionPlatform.get();
 	}
-
 }
